a71369f3ec72990e360e0a7c1d5f130f883b844d
Meziane <kmeziane2009@gmail.com>
2023-08-11 14:21:19 +0200

    Ajoutez des fichiers projet.

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Entrez un nombre inférieur à 100 : ");
+
+// variable
+string numberStr = Console.ReadLine();
+
+// conversion
+int number = Convert.ToInt32(numberStr);
+
+// Si le nombre est inférieur à 100 et pair
+if ((number < 100) && (number % 2 == 0))
+{
+    int sum = 0;
+    Console.WriteLine("Nombre pairs jusqu'à " + number + ": ");
+    for (int i = 2; i <= number; i = i + 2)
+    {        
+        Console.WriteLine(i);
+        sum += i;
+    }
+    Console.WriteLine("Somme = " + sum);
+}
+
+// Sinon, on vérifie si le nombre est supérieur ou égal à 100
+else if (number >= 100)
+{
+    Console.WriteLine("Le nombre doit être inférieur à 100");
+}
+
+// Sinon le nombre est impair
+else 
+{
+    Console.WriteLine("Le nombre doit être pair");
+}
+
